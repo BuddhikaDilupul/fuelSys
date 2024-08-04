459b6aff4750c3abcdbaadebeb299a94ccc7671d
Buddhika <buddhikadilupulr@gmail.com>
2024-08-04 18:03:17 +0530

    authentication added

--- pump-service/src/middlewares/authenticate.js
+++ pump-service/src/middlewares/authenticate.js
@@ -3,7 +3,7 @@ const secret = require('../config')
 exports.authenticate = (req, res, next) => {
   const token = req.headers.authorization.split(' ')[1];
   const decoded = jwt.decode(token);
-  const decodedToken = jwt.verify(token, secret);
+  const decodedToken = jwt.verify(token, secret.secret);
   if (decodedToken) {
     req.role = decoded.role;
     req.userId = decoded.id;
