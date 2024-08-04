40e70c9723307c648a3328bebc935cf7013a839d
Buddhika <buddhikadilupulr@gmail.com>
2024-08-04 17:39:59 +0530

    authentication added

--- daily-report-service/src/middlewares/authenticate.js
+++ daily-report-service/src/middlewares/authenticate.js
@@ -1,16 +1,17 @@
-const jwt = require("jsonwebtoken");
-
+const jwt = require('jsonwebtoken');
+const secret = require('../config')
 exports.authenticate = (req, res, next) => {
-  try {
-    const secret = process.env.secret;
-    const token = req.headers.authorization.split(" ")[1];
-    const decoded = jwt.decode(token);
-    jwt.verify(token, secret);
+  const token = req.headers.authorization.split(' ')[1];
+  const decoded = jwt.decode(token);
+  const decodedToken = jwt.verify(token, secret);
+  if (decodedToken) {
     req.role = decoded.role;
     req.userId = decoded.id;
     console.log(decoded.id, decoded.role);
     next();
-  } catch (error) {
-    res.status(400).json({ message: "invalid token" });
+  } else {
+    res.status(400).json({ message: 'invalid token' });
   }
-};
+
+
+}
