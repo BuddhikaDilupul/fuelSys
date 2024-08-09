bf42173c515e7173c49587b9cdeda1980e380a96
Buddhika <buddhikadilupulr@gmail.com>
2024-08-09 20:10:36 +0530

    bug fixing

--- pump-service/src/controllers/pump.controller.js
+++ pump-service/src/controllers/pump.controller.js
@@ -44,7 +44,7 @@ exports.updatePumpStateToInUse = async (req, res) => {
     if (!pumpCheckBeforeUse) {
       console.log(pumpCheckBeforeUse);
       
-      res.status(400).json({ message: "Already In use" });
+      return res.status(400).json({ message: "Already In use" });
     } else {
       const pumpCheck = await Pump.findByIdAndUpdate(
         req.params.id,
